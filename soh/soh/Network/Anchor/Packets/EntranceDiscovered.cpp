11829c1c0693dfe86f61c8eb3215ecb3878723e3
Sarge-117 <adam_branston@outlook.com>
2026-05-18 16:33:32 -0700

    tests

--- soh/soh/Network/Anchor/Packets/EntranceDiscovered.cpp
+++ soh/soh/Network/Anchor/Packets/EntranceDiscovered.cpp
@@ -34,7 +34,7 @@ void Anchor::HandlePacket_EntranceDiscovered(nlohmann::json payload) {
 
 void Anchor::SendPacket_EnterScene(s16 sceneNum) {
     if (!IsSaveLoaded()) {
-        return;
+//        return;
     }
 
     nlohmann::json payload;
