6a18b49719288fac13f83fdc16240bf9f9165f4c
Sarge-117 <adam_branston@outlook.com>
2026-05-19 14:43:16 -0700

    try another fix

--- soh/soh/Network/Anchor/Packets/GiveItem.cpp
+++ soh/soh/Network/Anchor/Packets/GiveItem.cpp
@@ -121,7 +121,7 @@ void Anchor::HandlePacket_GiveItem(nlohmann::json payload) {
             locationMessage = "";
         }
 
-        if (modId == MOD_RANDOMIZER && getItemId == RG_MASTER_SWORD && gPlayState->sceneNum == SCENE_TEMPLE_OF_TIME) {
+        if (suffix == "Master Sword" && info == "Market ToT Master Sword") {
             locationMessage = "";
             return;
         }
