59768e7364b34fba3ebc40a4c5d56336fa61fb1d
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-06-27 02:20:03 +0200

    Cleanup unused includes & remove remaining LUS umbrella includes (#6813)

--- soh/soh/Enhancements/randomizer/LockOverworldDoors.cpp
+++ soh/soh/Enhancements/randomizer/LockOverworldDoors.cpp
@@ -5,7 +5,6 @@
 
 extern "C" {
 extern PlayState* gPlayState;
-#include "macros.h"
 #include "src/overlays/actors/ovl_En_Door/z_en_door.h"
 #include "src/overlays/actors/ovl_Door_Shutter/z_door_shutter.h"
 #include "src/overlays/actors/ovl_En_Hintnuts/z_en_hintnuts.h"
