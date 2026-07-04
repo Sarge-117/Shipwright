b29fa3baeea4b799e05ac63d5ad0266ab796de31
Philip Dubé <159546+serprex@users.noreply.github.com>
2026-07-04 20:04:09 +0000

    Decouple GBK from LACS (#5838)
    Triforce Pieces can now be tokens for bridge or ganon's soul. & can be tokens for multiple rewards
    
    Wincon can now be arbitrary conditions
    
    Ganon's Soul (removed from existing boss soul options) can now be arbitrary conditions
    
    Co-authored-by: Pepper0ni <93387759+Pepper0ni@users.noreply.github.com>

--- soh/soh/Enhancements/randomizer/logic.h
+++ soh/soh/Enhancements/randomizer/logic.h
@@ -116,7 +116,9 @@ class Logic {
     bool CanShield();
     bool CanUseProjectile();
     bool CanBuildRainbowBridge();
-    bool CanTriggerLACS();
+    bool CanTriggerGBK();
+    bool CanTriggerGanonsSoul();
+    bool CanTriggerWincon();
     bool IsFireLoopLocked();
     bool ReachScarecrow();
     bool ReachDistantScarecrow();
@@ -145,6 +147,7 @@ class Logic {
     void SetRandoInf(uint32_t flag, bool state);
     bool CheckEventChkInf(int32_t flag);
     uint8_t GetGSCount();
+    uint8_t GetTriforcePieceCount();
     void SetEventChkInf(int32_t flag, bool state);
     uint8_t GetAmmo(uint32_t item);
     void SetAmmo(uint32_t item, uint8_t count);
