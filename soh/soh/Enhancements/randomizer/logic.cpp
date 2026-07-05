efac3dc6f9f2b4bf738906689d2ee6d1fafd2c20
Sarge-117 <adam_branston@outlook.com>
2026-07-04 23:36:07 -0700

    items & logic for deku and jabu boss keys

--- soh/soh/Enhancements/randomizer/logic.cpp
+++ soh/soh/Enhancements/randomizer/logic.cpp
@@ -216,7 +216,9 @@ bool Logic::HasItem(RandomizerGet itemName) {
         case RG_JABU_JABU_SMALL_KEY:
         case RG_ICE_CAVERN_SMALL_KEY:
         case RG_TOMB_SMALL_KEY:
+        case RG_DEKU_TREE_BOSS_KEY:
         case RG_DODONGOS_CAVERN_BOSS_KEY:
+        case RG_JABU_JABUS_BELLY_BOSS_KEY:
             return CheckRandoInf(RandoGetToRandInf.at(itemName));
             // Boss Keys
         case RG_FOREST_TEMPLE_BOSS_KEY:
@@ -1874,7 +1876,9 @@ std::map<RandomizerGet, uint32_t> Logic::RandoGetToRandInf = {
     { RG_JABU_JABU_SMALL_KEY, RAND_INF_JABU_SMALL_KEY_1_FOUND },
     { RG_ICE_CAVERN_SMALL_KEY, RAND_INF_ICE_SMALL_KEY_1_FOUND },
     { RG_TOMB_SMALL_KEY, RAND_INF_TOMB_SMALL_KEY_FOUND },
+    { RG_DEKU_TREE_BOSS_KEY, RAND_INF_DEKU_BOSS_KEY_FOUND },
     { RG_DODONGOS_CAVERN_BOSS_KEY, RAND_INF_DODONGO_BOSS_KEY_FOUND },
+    { RG_JABU_JABUS_BELLY_BOSS_KEY, RAND_INF_JABU_BOSS_KEY_FOUND },
 };
 
 std::map<uint32_t, uint32_t> Logic::RandoGetToDungeonScene = {
@@ -2331,7 +2335,9 @@ void Logic::ApplyItemEffect(Item& item, bool state) {
                 case RG_JABU_JABU_SMALL_KEY:
                 case RG_ICE_CAVERN_SMALL_KEY:
                 case RG_TOMB_SMALL_KEY:
+                case RG_DEKU_TREE_BOSS_KEY:
                 case RG_DODONGOS_CAVERN_BOSS_KEY:
+                case RG_JABU_JABUS_BELLY_BOSS_KEY:
                     SetRandoInf(RandoGetToRandInf.at(randoGet), state);
                     break;
                 case RG_TRIFORCE_PIECE:
