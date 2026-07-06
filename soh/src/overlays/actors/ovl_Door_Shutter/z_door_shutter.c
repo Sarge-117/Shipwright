a55a4fb59df1e9e90c40cc94d4ddbf06fd65c911
Sarge-117 <adam_branston@outlook.com>
2026-07-06 12:05:29 -0700

    don't set switch flag in ice cavern

--- soh/src/overlays/actors/ovl_Door_Shutter/z_door_shutter.c
+++ soh/src/overlays/actors/ovl_Door_Shutter/z_door_shutter.c
@@ -400,7 +400,9 @@ void DoorShutter_Idle(DoorShutter* this, PlayState* play) {
         DoorShutter_SetupAction(this, DoorShutter_Open);
         this->dyna.actor.velocity.y = 0.0f;
         if (this->unlockTimer != 0) {
-            Flags_SetSwitch(play, this->dyna.actor.params & 0x3F);
+            if (play->sceneNum != SCENE_ICE_CAVERN) {
+                Flags_SetSwitch(play, this->dyna.actor.params & 0x3F);
+            }
             if (this->doorType != SHUTTER_BOSS) {
                 if (play->sceneNum != SCENE_ROYAL_FAMILYS_TOMB) { // To avoid consuming a dodongo's cavern key when unlocking the door
                     gSaveContext.inventory.dungeonKeys[gSaveContext.mapIndex]--;
