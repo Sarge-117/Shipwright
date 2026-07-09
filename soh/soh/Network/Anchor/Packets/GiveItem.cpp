6b94c7d17a01dc098c5522d8564c55da3b9d211a
Sarge-117 <adam_branston@outlook.com>
2026-07-08 22:15:34 -0700

    hack for blue fire bottle playing wrong fanfare

--- soh/soh/Network/Anchor/Packets/GiveItem.cpp
+++ soh/soh/Network/Anchor/Packets/GiveItem.cpp
@@ -178,7 +178,7 @@ void Anchor::HandlePacket_GiveItem(nlohmann::json payload) {
         }
 
         if (getItemEntry.getItemCategory == ITEM_CATEGORY_MAJOR || getItemEntry.getItemCategory == ITEM_CATEGORY_BOSS_KEY) {
-            if (suffix != "Bombchu (5)") {
+            if (suffix != "Bombchu (5)" && suffix != "Bottle with Blue Fire") {
                 Audio_PlayFanfare_Rando(getItemEntry);
                 mute = true;
             }
