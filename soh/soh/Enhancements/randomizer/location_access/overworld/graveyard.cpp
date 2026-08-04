b8b04e0b44a4c388406e1322ccbee834afb93dc0
Sarge-117 <adam_branston@outlook.com>
2026-08-04 12:23:11 -0700

    CanOpenLargeChest for shrine

--- soh/soh/Enhancements/randomizer/location_access/overworld/graveyard.cpp
+++ soh/soh/Enhancements/randomizer/location_access/overworld/graveyard.cpp
@@ -75,7 +75,7 @@ void RegionTable_Init_Graveyard() {
         LOCATION(RC_GRAVEYARD_GLOMBON_CHEST_3,    logic->HasCustomFlame(RG_CUSTOM_FLAME_GREEN) && logic->HasItem(RG_OPEN_CHEST) && logic->CanUse(RG_ZELDAS_LULLABY) && logic->CanUse(RG_HOOKSHOT)),
         LOCATION(RC_GRAVEYARD_GLOMBON_CHEST_4,    logic->HasCustomFlame(RG_CUSTOM_FLAME_PURPLE) && logic->HasItem(RG_OPEN_CHEST) && logic->CanUse(RG_ZELDAS_LULLABY) && logic->CanUse(RG_HOOKSHOT)),
         LOCATION(RC_GRAVEYARD_GLOMBON_CHEST_5,    logic->HasCustomFlame(RG_CUSTOM_FLAME_ORANGE) && logic->HasCustomFlame(RG_CUSTOM_FLAME_BLUE) && logic->HasCustomFlame(RG_CUSTOM_FLAME_GREEN) && logic->HasCustomFlame(RG_CUSTOM_FLAME_PURPLE) 
-                                               && logic->HasItem(RG_OPEN_CHEST) && logic->CanUse(RG_ZELDAS_LULLABY) && logic->CanUse(RG_HOOKSHOT)),
+                                               && logic->CanOpenLargeChest() && logic->CanUse(RG_ZELDAS_LULLABY) && logic->CanUse(RG_HOOKSHOT)),
     }, {
         //Exits
         ENTRANCE(RR_GRAVEYARD_SHIELD_GRAVE, true),
